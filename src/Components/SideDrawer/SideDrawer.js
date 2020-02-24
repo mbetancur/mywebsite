628bfe1f5489f5c0f2d5ae6a1518fe3ed1be8b70
mateo <slash-252@hotmail.com>
2020-02-23 19:51:19 -0500

    styling lateral navbar

--- src/Components/SideDrawer/SideDrawer.js
+++ src/Components/SideDrawer/SideDrawer.js
@@ -8,14 +8,26 @@ const SideDrawer = props => {
         drawerClasses = 'side-drawer open'
     };
     return (
-        <nav className={drawerClasses}>
-            <ul><li>
-                <a href="/">Products</a>
-            </li>
-                <li>
-                    <a href="/">Users</a>
-                </li></ul>
-        </nav>
+        <div className={drawerClasses}>
+            <div className="my-photo">
+                <img className="small-photo" alt="myphoto"src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRf_bRDRhLHjTnzUjKBR-Czexw2yuYow4YjNQxOcLguCHzVATgZ"></img>
+            </div>
+            <div className="my-name">Mateo Betancur</div>
+            <nav >
+                <ul className="site-nav">
+                    <li>
+                        <a href="/">Products</a>
+                    </li>
+                    <li>
+                        <a href="/">Users</a>
+                    </li>
+                </ul>
+            </nav>
+            <div className="social-links">
+                <i className="fa fa-linkedin"></i>
+                <i className="fa fa-twitter"></i>
+            </div>
+        </div>
     );
 };
 
